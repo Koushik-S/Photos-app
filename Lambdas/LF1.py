3407063b515b5c5c18a352e3150a5414b6355b71
Koushik Srinivasula <56915969+Koushik-S@users.noreply.github.com>
2023-03-30 21:26:39 -0400

    Update LF1.py

--- Lambdas/LF1.py
+++ Lambdas/LF1.py
@@ -5,15 +5,20 @@ from opensearchpy import OpenSearch, RequestsHttpConnection
 
 def lambda_handler(event, context):
     # Get the S3 bucket and key from the event
+
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
-    
+    print(bucket,key)
     # Get the customLabels metadata from the S3 object
     s3 = boto3.client('s3')
+    print(s3.head_object(Bucket=bucket, Key=key))
     metadata = s3.head_object(Bucket=bucket, Key=key)['Metadata']
-    custom_labels = metadata.get('x-amz-meta-customLabels')
+    print(metadata)
+    custom_labels = metadata.get('customlabels')
     if custom_labels:
-        labels = json.loads(custom_labels)
+        labels=[]
+        for L in custom_labels.split(","):
+            labels.append(L)
     else:
         labels = []
     
@@ -40,7 +45,7 @@ def lambda_handler(event, context):
     }
     
     service = 'es'
-    #https://search-photos-lbffn7ibyl5cnd5eqf3b7zeqvy.us-east-1.es.amazonaws.com/
+    host = 'search-restaurant-g56zesqy2cp7vled5m5ivgpep4.us-east-1.es.amazonaws.com'
     region = 'us-east-1'
     credentials = boto3.Session().get_credentials()
     awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
